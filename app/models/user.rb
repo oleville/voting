2447650077a6d324a84a9b22b5503c3811d40624
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-01 15:27:15 -0600

    Take user email information and use that to find user as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 	has_many :votes
 
 	def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+		where(provider: auth.provider, uid: auth.uid, email: auth.info.email).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
 			user.name = auth.info.name
