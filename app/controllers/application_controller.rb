187bc37aae392e96ed67d876c7021571f08f76d9
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-02 21:01:35 -0600

    Disallow expired tokens from doing stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 	def current_user
 		begin
 			@current_user ||= User.find(session[:user_id]) if session[:user_id]
+
+			if Time.at(@current_user.auth_token_expiration) <= Time.now
+				@current_user = nil
+			end
+
+			return @current_user
 		rescue
 			@current_user = nil
 		end
