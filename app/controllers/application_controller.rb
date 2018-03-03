471444163f62c9e13827db3f30eae26201eb5429
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-02 21:04:14 -0600

    Add private flag for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+	private
+
 	def current_user
 		begin
 			@current_user ||= User.find(session[:user_id]) if session[:user_id]
