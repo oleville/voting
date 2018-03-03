2f655d959fe7a6f96a238b7b4f384f6f6fd3cf7e
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-02 21:15:48 -0600

    Reindent CandidatesController

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,85 +1,85 @@
 class CandidatesController < ApplicationController
-  before_action :set_candidate, only: [:show, :edit, :update, :destroy]
+	before_action :set_candidate, only: [:show, :edit, :update, :destroy]
 
-  # GET /candidates
-  # GET /candidates.json
+	# GET /candidates
+	# GET /candidates.json
 	def index
 		require_login!
 		@candidates = Candidate.all
-  end
+	end
 
-  # GET /candidates/1
-  # GET /candidates/1.json
+	# GET /candidates/1
+	# GET /candidates/1.json
 	def show
 		require_login!
-  end
+	end
 
-  # GET /candidates/new
+	# GET /candidates/new
 	def new
 		require_admin!
-    @candidate = Candidate.new
-  end
+		@candidate = Candidate.new
+	end
 
-  # GET /candidates/1/edit
+	# GET /candidates/1/edit
 	def edit
 		require_admin!
-  end
+	end
 
-  # POST /candidates
-  # POST /candidates.json
+	# POST /candidates
+	# POST /candidates.json
 	def create
 		require_admin!
 
-    @candidate = Candidate.new(candidate_params)
+		@candidate = Candidate.new(candidate_params)
 
-    respond_to do |format|
-      if @candidate.save
-        format.html { redirect_to @candidate, notice: 'Candidate was successfully created.' }
-        format.json { render :show, status: :created, location: @candidate }
-      else
-        format.html { render :new }
-        format.json { render json: @candidate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @candidate.save
+				format.html { redirect_to @candidate, notice: 'Candidate was successfully created.' }
+				format.json { render :show, status: :created, location: @candidate }
+			else
+				format.html { render :new }
+				format.json { render json: @candidate.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /candidates/1
-  # PATCH/PUT /candidates/1.json
+	# PATCH/PUT /candidates/1
+	# PATCH/PUT /candidates/1.json
 	def update
 		require_admin!
 
-    respond_to do |format|
-      if @candidate.update(candidate_params)
-        format.html { redirect_to @candidate, notice: 'Candidate was successfully updated.' }
-        format.json { render :show, status: :ok, location: @candidate }
-      else
-        format.html { render :edit }
-        format.json { render json: @candidate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @candidate.update(candidate_params)
+				format.html { redirect_to @candidate, notice: 'Candidate was successfully updated.' }
+				format.json { render :show, status: :ok, location: @candidate }
+			else
+				format.html { render :edit }
+				format.json { render json: @candidate.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /candidates/1
-  # DELETE /candidates/1.json
-  def destroy
-	  require_admin!
+	# DELETE /candidates/1
+	# DELETE /candidates/1.json
+	def destroy
+		require_admin!
 
-	  @candidate.destroy
-    respond_to do |format|
-      format.html { redirect_to candidates_url, notice: 'Candidate was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+		@candidate.destroy
+		respond_to do |format|
+			format.html { redirect_to candidates_url, notice: 'Candidate was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
 
-  private
+	private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_candidate
-	  @candidate = Candidate.find(params[:id])
-  end
+	# Use callbacks to share common setup or constraints between actions.
+	def set_candidate
+		@candidate = Candidate.find(params[:id])
+	end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def candidate_params
-	  params.require(:candidate).permit(:name, :picture_url, :description, :violations, :election_id, :position_id)
-  end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def candidate_params
+		params.require(:candidate).permit(:name, :picture_url, :description, :violations, :election_id, :position_id)
+	end
 end
