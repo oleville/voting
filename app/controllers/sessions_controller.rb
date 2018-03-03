e5880fbc3bd8f475df3d6569fa70b93186d0aeb7
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-02 21:16:27 -0600

    Revoke user token on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,20 @@ class SessionsController < ApplicationController
   end
 
 	def destroy
+		if user = current_user
+			begin
+				puts "Revoking token for user #{user.id}"
+
+				uri = URI('https://accounts.google.com/o/oauth2/revoke')
+				params = { :token => current_user.auth_token }
+				uri.query = URI.encode_www_form(params)
+				response = Net::HTTP.get(uri)
+			rescue RuntimeError => e
+				puts e
+			end
+		end
+
 		session[:user_id] = nil
-    redirect_to root_path
+		redirect_to root_path
   end
 end
