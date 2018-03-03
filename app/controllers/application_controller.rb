e4c0f36c27b863434228e8162bc334bdf1b66ce7
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-02 21:02:33 -0600

    Add a couple of helper methods to clean up access control
    - require_login! will redirect to the root url if the current user
      does not exist, and
    
    - require_admin! will do the same but with a notice.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,13 @@ class ApplicationController < ActionController::Base
 		end
 	end
 
+	def require_login!
+		redirect_to :root if current_user.nil?
+	end
+
+	def require_admin!
+		if !current_user || !current_user.admin?
+			redirect_to :root, notice: "Not allowed to access this resource."
+		end
+	end
 end
