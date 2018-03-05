a24991b4ae3c2201d09153211b3eaea5e3bb3c97
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-05 16:27:09 -0600

    Delete edit route on ballot

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 	resources :sessions, only: [:create, :destroy]
 	resources :home, only: [:show]
 
-  resources :ballots
+  resources :ballots, except: [:edit]
   resources :votes
   resources :positions
   resources :candidates
