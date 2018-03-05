243e2f37c932fe679bc93db423e7c739d79cb95f
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-05 11:13:58 -0600

    Redirect straight to new ballot page if only one open election
    This was remarkably simple to implement.  Yay, Rails!
    
    [GitHub] This commit closes #8.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
 	def show
 		@votable_elections = Election.currently_open_to(@current_user)
+
+		if @votable_elections.count == 1 && !current_user.is_admin?
+			redirect_to new_ballot_url(params: {election_id: @votable_elections.first.id})
+		end
   end
 end
