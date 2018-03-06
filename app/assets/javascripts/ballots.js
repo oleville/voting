7d46efc934abadba15c50d1703ec378b3487ae5b
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-05 19:53:58 -0600

    Add some stupid logic to make people not be able to select stuffies
    [GitHub] This commit resolves #7.

--- app/assets/javascripts/ballots.js
+++ app/assets/javascripts/ballots.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+const hooky = () => {
+	[].slice.call(document.querySelectorAll('.position')).forEach((position) => {
+		console.log(`Inserting hooks for ${position}`);
+
+		[].slice.call(position.querySelectorAll('.candidate .selector input')).forEach((input) => {
+			input.onclick = () => {
+				let rankChoice = input.getAttribute('id').toString().match(/_rank_(\d+)/)[0];
+				let otherRanksForThisPosition = [].slice.call(position.querySelectorAll(`[id$=${rankChoice}]`)).filter(item => item !== input )
+
+				otherRanksForThisPosition.forEach(element => element.disabled = true)
+			}
+		})
+	})
+}
+
+window.onload = () => { hooky(); }
