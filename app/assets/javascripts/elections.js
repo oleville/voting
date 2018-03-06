9f93b518456f33d7a31c8253d55f1ab47c5d7c13
Kristofer Rye <kristofer.rye@gmail.com>
2018-03-06 08:56:54 -0600

    Add an event listener to reload live page

--- app/assets/javascripts/elections.js
+++ app/assets/javascripts/elections.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+window.addEventListener("turbolinks:load", () => {
+	if(window.location.href.match(/\/elections\/\d+\/live/))
+		setInterval(() => {
+			window.location.reload(true)
+		}, 10000)
+})
